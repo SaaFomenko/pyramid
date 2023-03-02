407e3218e297c34561e01ee4266db39a5f78aec6
SaaFomenko <s.fomenko@list.ru>
2023-03-02 20:46:43 +0300

    Need test print_pyramid()

--- pyramid.cpp
+++ pyramid.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include "pyramid.h"
 
+static const char* root_lable = " root ";
+static const char* left_lable = " left(";
+static const char* right_lable = " right(";
 
-void print_pyramid(Pyramid pyramid)
-{
+void print_pyramid(Pyramid& pyramid, int index)
+{ 
+	if (index < 0)
+	{
+		return;
+	}
 
+	if (index == 0)
+	{
+		std::cout << index << root_lable << pyramid.root() << std::endl;
+	}
+	else
+	{
+		int left_index = pyramid.left_index(index);
+		if (left_index > 0)
+		{
+			std::cout << pyramid.level(index) << left_lable << 
+				pyramid.from_index(index) << ") " << 
+				pyramid.from_index(left_index) << std::endl;
+			print_pyramid(pyramid, left_index);
+		}
+
+		int right_index = pyramid.right_index(index);
+		if (right_index > 0)
+		{
+			std::cout << pyramid.level(index) << right_lable << 
+				pyramid.from_index(index) << ") " << 
+				pyramid.from_index(right_index) << std::endl;
+			print_pyramid(pyramid, right_index);
+		}
+	}
 }
 
 Pyramid::Pyramid(int size, int arr_first, ...) : 
@@ -14,15 +45,12 @@ Pyramid::Pyramid(int size, int arr_first, ...) :
 //	arr = &arr_first;
 }
 
+Pyramid::~Pyramid()
+{}	
+
 int Pyramid::root()
 {
 	return arr[0];
-}
-
-int Pyramid::from_index(int index)
-{
-	valid_index(index);
-	return arr[index];
 }
 
 void Pyramid::valid_index(int index)
@@ -31,4 +59,59 @@ void Pyramid::valid_index(int index)
 	{
 		throw "Error, pyramid index fail!";
 	}
+}
+
+int Pyramid::from_index(int index)
+{
+	valid_index(index);
+	return arr[index];
+}
+
+int Pyramid::left_index(int parent_index)
+{
+	valid_index(parent_index);
+
+	int index = 2 * parent_index + 1;
+
+	if (index >= _size)	
+		return -1;
+
+	return index;
+}
+
+int Pyramid::right_index(int parent_index)
+{
+	valid_index(parent_index);
+	
+	int index = 2 * parent_index + 2;
+
+	if (index >= _size)
+		return -1;
+	
+	return index;
+}
+
+int Pyramid::parent_index(int child_index)
+{
+	valid_index(child_index);
+	
+	return (child_index - 1) / 2;
+}
+
+int Pyramid::level(int index)
+{
+	valid_index(index);
+
+	if (index == 0)
+		return 0;
+
+	int i = 0;
+
+	while (index > 0)
+	{
+		index = parent_index(index);
+		++i;
+	}
+
+	return i + 1;
 }
--- pyramid.h
+++ pyramid.h
@@ -10,16 +10,17 @@ class Pyramid
 
 	public:
 		int root();
-		int from_index(int index);
-		int left_index(int index);
-		int right_index(int index);
-		int parent_index(int index);
 		void valid_index(int index);
+		int from_index(int index);
+		int left_index(int parent_index);
+		int right_index(int parent_index);
+		int parent_index(int child_index);
+		int level(int index);
 
 		Pyramid(int size, int arr_first, ...);
 		~Pyramid();
 };
 
-void print_pyramid(Pyramid& pyramid);
+void print_pyramid(Pyramid& pyramid, int index);
 
 #endif
